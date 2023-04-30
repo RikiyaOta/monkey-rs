28e4af5bfc1cd3a16bd021b9b67eb5b8e8a608de
rikiyaota <rikiya0421.fcb6@gmail.com>
2023-04-30 16:06:47 +0900

    Refactor. Define a new enum type.

--- src/token.rs
+++ src/token.rs
@@ -1,22 +1,23 @@
-pub type TokenType = String;
+#[derive(Debug, PartialEq)]
+pub enum TokenType {
+    ILLEGAL,
+    EOF,
+    IDENT,
+    INT,
+    ASSIGN,
+    PLUS,
+    COMMA,
+    SEMICOLON,
+    LPAREN,
+    RPAREN,
+    LBRACE,
+    RBRACE,
+    FUNCTION,
+    LET,
+}
 
 #[derive(Debug)]
 pub struct Token {
     pub r#type: TokenType,
     pub literal: String,
 }
-
-pub const ILLEGAL: &str = "ILLEGAL";
-pub const EOF: &str = "EOF";
-pub const IDENT: &str = "IDENT";
-pub const INT: &str = "INT";
-pub const ASSIGN: &str = "=";
-pub const PLUS: &str = "+";
-pub const COMMA: &str = ",";
-pub const SEMICOLON: &str = ";";
-pub const LPAREN: &str = "(";
-pub const RPAREN: &str = ")";
-pub const LBRACE: &str = "{";
-pub const RBRACE: &str = "}";
-pub const FUNCTION: &str = "FUNCTION";
-pub const LET: &str = "LET";
