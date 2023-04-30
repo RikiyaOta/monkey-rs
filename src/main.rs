32e97b787075f2ea09b5249cac8dbf5ffd442227
rikiyaota <rikiya0421.fcb6@gmail.com>
2023-04-30 18:45:55 +0900

    Finish implementing my first lexer and its repl.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
 pub mod lexer;
-pub mod token;
 pub mod repl;
+pub mod token;
+
+use lexer::Lexer;
+use token::TokenType;
 
 fn main() {
     loop {
         let user_input = repl::start_repl().unwrap();
-        println!("user_input: {}", user_input);
+        let mut lexer = Lexer::new(user_input);
+
+        loop {
+            let token = lexer.next_token();
+            if token.r#type == TokenType::EOF {
+                break;
+            } else {
+                println!("{:?}", token);
+            }
+        }
     }
 }
