0e0cf4172f5e464a0410fd1b47d97f03a864f2b4
rikiyaota <rikiya0421.fcb6@gmail.com>
2023-04-30 18:45:55 +0900

    Finish implementing my first lexer and its repl.

--- src/repl.rs
+++ src/repl.rs
@@ -8,6 +8,6 @@ pub fn start_repl() -> Result<String, io::Error> {
     let stdin = io::stdin();
     match stdin.read_line(&mut user_input) {
         Ok(_) => Ok(user_input),
-        Err(error) => Err(error)
+        Err(error) => Err(error),
     }
 }
